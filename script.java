e1abc75a0806cfd4febbacc6cda890ab328f780e
Grigol <153779807+GrigolPortfolio@users.noreply.github.com>
2023-12-17 17:40:11 -0500

    .

--- script.java
+++ script.java
@@ -1,2 +1,9 @@
 // script.js
 console.log("Hello, Engineering World!");
+
+//3D Effect
+
+window.addEventListener('scroll', function() {
+  var header = document.querySelector('header');
+  header.classList.toggle('scroll', window.scrollY > 0);
+});
