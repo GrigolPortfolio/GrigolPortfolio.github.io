9a987a830f0992a28dcc661feb801fd494358a95
Grigol <153779807+GrigolPortfolio@users.noreply.github.com>
2023-12-20 02:31:37 -0500

    /

--- script.java
+++ script.java
@@ -1,27 +1,11 @@
 // script.js
 
 // Text for typing animation
-const textToType = "This should be typing live....wow";
-
-// Time delay between each character (in milliseconds)
-const typingSpeed = 100;
-
-// Function to simulate typing animation
-function typeWriterEffect() {
-    let index = 0;
-    const headerText = document.getElementById('typing-text');
-    const textLength = textToType.length;
-
-    function type() {
-        if (index < textLength) {
-            headerText.textContent += textToType.charAt(index);
-            index++;
-            setTimeout(type, typingSpeed);
-        }
-    }
-
-    type();
-}
+<div class="wrapper">
+    <div class="typing-demo">
+      This should be typing live....wow
+    </div>
+</div>
 
 // Start typing animation when the page loads
 window.onload = typeWriterEffect;
