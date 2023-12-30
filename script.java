f286b8f1fc8a4f4f2722a95865dfccad4c1bd123
Grigol <153779807+GrigolPortfolio@users.noreply.github.com>
2023-12-30 17:06:48 -0500

    Update script.java

--- script.java
+++ script.java
@@ -19,3 +19,35 @@ const typeEffect = () => {
 };
 
 document.addEventListener('DOMContentLoaded', typeEffect);
+
+//___________________3D Model__________________________//
+// Create a scene
+const scene = new THREE.Scene();
+
+// Create a camera
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 5;
+
+// Create a renderer
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+// Append the renderer's DOM element to your container
+const container = document.getElementById('model-container');
+container.appendChild(renderer.domElement);
+
+// Load the STL file and render the model in the scene
+const loader = new THREE.STLLoader();
+loader.load('path_to_your_model.stl', function (geometry) {
+    const material = new THREE.MeshNormalMaterial();
+    const mesh = new THREE.Mesh(geometry, material);
+    scene.add(mesh);
+    renderer.render(scene, camera);
+});
+
+// Animate/render loop
+function animate() {
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+}
+animate();
