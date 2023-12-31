47ac6ab5e6d8b6b1f641640c04586df9c4e6b5f5
Grigol <153779807+GrigolPortfolio@users.noreply.github.com>
2023-12-30 19:38:07 -0500

    Update script.js

--- script.js
+++ script.js
@@ -20,34 +20,44 @@ const typeEffect = () => {
 
 document.addEventListener('DOMContentLoaded', typeEffect);
 
-//___________________3D Model__________________________//
-// Create a scene
-const scene = new THREE.Scene();
 
-// Create a camera
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.z = 5;
+//_____________________CODE FOR 3D VIDEO______________________________
 
-// Create a renderer
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
+const video = document.getElementById('scrollVideo');
+const videoContainer = document.getElementById('videoContainer');
 
-// Append the renderer's DOM element to your container
-const container = document.getElementById('model-container');
-container.appendChild(renderer.domElement);
-
-// Load the STL file and render the model in the scene
-const loader = new THREE.STLLoader();
-loader.load('Nuts_and_Bolts_Template(1).stl', function (geometry) {
-    const material = new THREE.MeshNormalMaterial();
-    const mesh = new THREE.Mesh(geometry, material);
-    scene.add(mesh);
-    renderer.render(scene, camera);
-});
-
-// Animate/render loop
-function animate() {
-    requestAnimationFrame(animate);
-    renderer.render(scene, camera);
+// Function to check if an element is in the viewport
+function isInViewport(element) {
+    const rect = element.getBoundingClientRect(); 
+    return (
+        rect.top >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+    );
 }
-animate();
+
+// Function to handle video playback based on scroll
+function handleVideoPlayback() {
+    if (isInViewport(videoContainer)) {
+        video.play();
+    } else {
+        video.pause();
+    }
+}
+
+// Event listener for scroll
+window.addEventListener('scroll', handleVideoPlayback);
+
+// Initial check when the page loads
+handleVideoPlayback();
+
+//__________________CODE FOR 3D MODEL IMPORT__________________
+
+document.addEventListener('DOMContentLoaded', function () {
+    // When the DOM content is loaded
+
+    // Get the entity (3D model) by its ID
+    var modelEntity = document.querySelector('#yourModelID'); // Replace 'yourModelID' with the ID of your 3D model entity
+
+    // Manipulate the position of the model
+    modelEntity.setAttribute('position', { x: 1, y: 2, z: -3 }); // Modify the position (change x, y, z values)
+});
