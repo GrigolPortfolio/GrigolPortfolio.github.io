b48c12aea57a11cd5cba5a769d6c75ec7a62cdce
Grigol <153779807+GrigolPortfolio@users.noreply.github.com>
2024-03-05 18:46:58 -0500

    Update scriptjava.js

--- scriptjava.js
+++ scriptjava.js
@@ -3,7 +3,7 @@ window.addEventListener('scroll', function() {
     const whiteOverlay = document.getElementById('white-overlay');
     const scrollPosition = window.scrollY;
     const fadeStart = 0; // Start fading at the top of the page
-    const fadeEnd = 4400; // Scroll down 400px to fully fade out
+    const fadeEnd = 4000; // Scroll down 400px to fully fade out
     let opacity;
 
     // Calculate opacity based on scroll position
